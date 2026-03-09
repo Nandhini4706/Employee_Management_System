b08aab56d178410d85eef2eefba0e5429003d7d1
Nandhini M <nandhinimm4706@gmail.com>
2026-03-10 00:21:34 +0530

    DBConnection

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 import db.DBConnection;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.*;
 public class Main{
     public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
         Connection con= DBConnection.getConnection();
-        if(con != null){
+     /*   if(con != null){
             System.out.println("Connected Successfully");
         }else{
             System.out.println("Connection Failed");
         }
+
+      */
+        String query ="Insert into employee(id,name,age,department,designation,reporting_to)" +
+                "values(?,?,?,?,?,?)";
+        System.out.println("1.enter id");
+        int id=sc.nextInt();
+        PreparedStatement ps= con.prepareStatement(query);
+        ps.setInt(1,id);
     }
 }
