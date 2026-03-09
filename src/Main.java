af76c6d1b87fb13438c5ad81935669768d6b4c3b
Nandhini M <nandhinimm4706@gmail.com>
2026-03-10 00:30:29 +0530

    DBConnection

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,24 @@ public class Main{
         String query ="Insert into employee(id,name,age,department,designation,reporting_to)" +
                 "values(?,?,?,?,?,?)";
         System.out.println("1.enter id");
-        int id=sc.nextInt();
+        int id=sc.nextInt(); sc.nextLine();
+        System.out.println("2. enter name");
+        String name=sc.nextLine();
+        System.out.println("3.enter age");
+        int age=sc.nextInt();sc.nextLine();
+        System.out.println("4.enter department");
+        String dept=sc.nextLine();
+        System.out.println("5.designation");
+        String des=sc.nextLine();
+        System.out.println("6.reporting to: ");
+        String repo=sc.nextLine();
         PreparedStatement ps= con.prepareStatement(query);
         ps.setInt(1,id);
+        ps.setString(2,name);
+        ps.setInt(3,age);
+        ps.setString(4,dept);
+        ps.setString(5,des);
+        ps.setString(6,repo);
+
     }
 }
