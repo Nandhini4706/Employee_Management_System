3b228fb0b7b1deb9052ffa1efb963660219ee752
Nandhini M <nandhinimm4706@gmail.com>
2026-03-11 15:25:41 +0530

    menu driven

--- src/Service/EmployeeService.java
+++ src/Service/EmployeeService.java
@@ -1,5 +1,19 @@
 package Service;
 
-public class EmployeeService {
+import Model.Employee;
 
+import java.util.ArrayList;
+
+public class EmployeeService {
+        ArrayList<Employee> emp;
+
+    public EmployeeService(ArrayList<Employee> emp) {
+        this.emp = emp;
+    }
+    public void viewDetails(){
+        for(Employee e:emp){
+            e.display();
+
+        }
+    }
 }
