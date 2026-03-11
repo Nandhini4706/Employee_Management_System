3d3844c51be4c524dc1f9ef70946bc76141cba83
Nandhini M <nandhinimm4706@gmail.com>
2026-03-11 14:11:52 +0530

    menu driven

--- src/Main.java
+++ src/Main.java
@@ -13,27 +13,37 @@ public class Main{
         }
 
       */
-        String query ="Insert into employee(id,name,age,department,designation,reporting_to)" +
-                "values(?,?,?,?,?,?)";
-        System.out.println("1.enter id");
-        int id=sc.nextInt(); sc.nextLine();
-        System.out.println("2. enter name");
-        String name=sc.nextLine();
-        System.out.println("3.enter age");
-        int age=sc.nextInt();sc.nextLine();
-        System.out.println("4.enter department");
-        String dept=sc.nextLine();
-        System.out.println("5.designation");
-        String des=sc.nextLine();
-        System.out.println("6.reporting to: ");
-        String repo=sc.nextLine();
-        PreparedStatement ps= con.prepareStatement(query);
-        ps.setInt(1,id);
-        ps.setString(2,name);
-        ps.setInt(3,age);
-        ps.setString(4,dept);
-        ps.setString(5,des);
-        ps.setString(6,repo);
+        while(true) {
+            String query = "Insert into employee(id,name,age,department,designation,reporting_to)" +
+                    "values(?,?,?,?,?,?)";
 
+            System.out.println("-----------------------------------Menu------------------------------------------");
+            System.out.println("1.Add Employee");
+            System.out.println("2.View Employee");
+            System.out.println("3.Exit");
+            System.out.println("---------------------------------------------------------------------------------");
+
+            System.out.println("1.enter id");
+            int id = sc.nextInt();
+            sc.nextLine();
+            System.out.println("2. enter name");
+            String name = sc.nextLine();
+            System.out.println("3.enter age");
+            int age = sc.nextInt();
+            sc.nextLine();
+            System.out.println("4.enter department");
+            String dept = sc.nextLine();
+            System.out.println("5.designation");
+            String des = sc.nextLine();
+            System.out.println("6.reporting to: ");
+            String repo = sc.nextLine();
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, id);
+            ps.setString(2, name);
+            ps.setInt(3, age);
+            ps.setString(4, dept);
+            ps.setString(5, des);
+            ps.setString(6, repo);
+        }
     }
 }
